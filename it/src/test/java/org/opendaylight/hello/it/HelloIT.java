b3124f1f7c84b432de14edd2e7d1c1b5a9386fe6
flaviof <ffernand@redhat.com>
2015-08-23 15:56:40 +0100

    exercise 13

--- it/src/test/java/org/opendaylight/hello/it/HelloIT.java
+++ it/src/test/java/org/opendaylight/hello/it/HelloIT.java
@@ -18,11 +18,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.rev150105.*;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.rev150105.greeting.registry.GreetingRegistryEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.rev150105.greeting.registry.GreetingRegistryEntryBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hello.rev150105.greeting.registry.GreetingRegistryEntryKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.RpcResult;
@@ -85,6 +88,36 @@ public class HelloIT extends AbstractMdsalTestBase {
     @Test
     public void testRPC() throws InterruptedException, ExecutionException {
         final String name = "bla bla bla";
+        final String expectedGreeting = "Hello " + name;
+        validateRPCResponse(name, expectedGreeting);
+        validateGreetingRegistry(name, expectedGreeting);
+    }
+
+    @Test
+    public void testProgrammableRPC() throws InterruptedException, ExecutionException, TransactionCommitFailedException {
+        final String name = "Colin Dixon";
+        final String defaultResponse = "Hello " + name;
+        final String response = "Hola " + name;
+        validateRPCResponse(name, defaultResponse);
+        programResponse(name,response);
+        validateGreetingRegistry(name, response);
+    }
+
+    private void programResponse(String name, String response) throws TransactionCommitFailedException {
+        DataBroker db = getSession().getSALService(DataBroker.class);
+        WriteTransaction transaction = db.newWriteOnlyTransaction();
+        InstanceIdentifier<GreetingRegistryEntry> iid = InstanceIdentifier.create(GreetingRegistry.class)
+                .child(GreetingRegistryEntry.class, new GreetingRegistryEntryKey(name));
+        GreetingRegistryEntry entry = new GreetingRegistryEntryBuilder()
+                .setName(name)
+                .setGreeting(response)
+                .build();
+        transaction.put(LogicalDatastoreType.CONFIGURATION, iid, entry);
+        CheckedFuture<Void, TransactionCommitFailedException> future = transaction.submit();
+        future.checkedGet();
+    }
+
+    private void validateRPCResponse(String name, String response) throws InterruptedException, ExecutionException {
         HelloService service = getSession().getRpcService(HelloService.class);
 
         HelloWorldInput input = new HelloWorldInputBuilder()
@@ -93,12 +126,11 @@ public class HelloIT extends AbstractMdsalTestBase {
         Future<RpcResult<HelloWorldOutput>> outputFuture = service.helloWorld(input);
         RpcResult<HelloWorldOutput> outputResult = outputFuture.get();
         Assert.assertTrue("RPC was unsuccessful", outputResult.isSuccessful());
-        Assert.assertEquals("Did not receive the expected response to helloWorld RPC", "Hello " + name,
+        Assert.assertEquals("Did not receive the expected response to helloWorld RPC", response,
                 outputResult.getResult().getGreeting());
-        validateGreetingRegistry(name);
     }
 
-    private void validateGreetingRegistry(String name) {
+    private void validateGreetingRegistry(String name, String expectedGreeting) {
         InstanceIdentifier<GreetingRegistryEntry> iid = InstanceIdentifier.create(GreetingRegistry.class)
                 .child(GreetingRegistryEntry.class, new GreetingRegistryEntryKey(name));
         DataBroker db = getSession().getSALService(DataBroker.class);
@@ -113,7 +145,8 @@ public class HelloIT extends AbstractMdsalTestBase {
         }
         Assert.assertTrue(name + " not recorded in greeting registry", optional.isPresent());
         Assert.assertEquals("Data store has unexpected name", name, optional.get().getName());
-        Assert.assertEquals("Did not receive the expected greeting in to helloWorld data store", "Hello " + name,
+        Assert.assertEquals("Did not receive the expected greeting in to helloWorld data store",
+                expectedGreeting,
                 optional.get().getGreeting());
     }
 }
